bfb7d12f92172b6ab4eea1284dad3f937870de97
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-02 00:05:50 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -89,11 +89,11 @@ class Administrator(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def edit(self,ctx, *,message_id,content:str):
+    async def edit(self,ctx,channel_id,message_id,content:str):
         try:
-            channel = ctx.channel
+            channel = await self.client.fetch_channel(channel_id)
             message = await channel.fetch_message(message_id)
-            await message.edit(content)
+            await message.edit(content=content)
         except discord.NotFound as e:
             await ctx.send("Could not find that message")
             raise e
