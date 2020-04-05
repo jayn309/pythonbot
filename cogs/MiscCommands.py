772e7b98e699a88e4b02c0214c683bc3b05c658b
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-04 17:33:32 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -159,7 +159,7 @@ class MicsCommands(commands.Cog):
             await ctx.send("<:ireneyikes:679733703647559720> What a loser!")
     @numgame.error
     async def numgame_error(self,ctx, error):
-        if isinstance(error, commands.CheckFailure):
+        if isinstance(error, commands.UserInputError):
             await ctx.send("Invalid input.Plese restart the game.")
                 
 
