9d42a1f2971d7d9ff78040102135938e15bdec6e
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-05 16:38:01 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -168,35 +168,42 @@ class MicsCommands(commands.Cog):
     async def rps(self,ctx, msg: str):
         t = ["rock","paper","scissors"]
         computer = t[randint(0, 2)]
-        win = ["<a:wendyhype:696114546850529341>" , "<a:irenelikeit:696142888500985896>" , "<a:wendyshrug:696150254441201674>" , "<:wendysip:681749452859506696>"]
-        lose = ["<a:wendyspeechless:684122984801107983>" , "<:wendypleading:695287540617445446>" , "<:irenepout:683431934860591128>" , "<a:irenefreeze:696114438092095489>"]
-        huh = ["<:wendywhat:681337728910098434>" , "<:seulgisquint:683715248645210152>" , "<:irenejudge:685426997224144916>" , "<:irenemunch:685426997337653258>"]
+        win = ["<a:wendyhype:696114546850529341>" , "<a:irenelikeit:696142888500985896>" , "<a:wendyshrug:696150254441201674>" , "<:wendysip:681749452859506696>", "<a:wenrenelaugha:683489034026221629>"]
+        lose = ["<a:wendyspeechless:684122984801107983>" , "<:wendypleading:695287540617445446>" , "<:irenepout:683431934860591128>" , "<a:irenefreeze:696114438092095489>", "<:wendyfist:684275169585528852> "]
+        huh = ["<:wendywhat:681337728910098434>" , "<:seulgisquint:683715248645210152>" , "<:irenejudge:685426997224144916>" , "<:irenemunch:685426997337653258>", "<a:wenrenefeedinga:683682968828379170><a:wenrenefeedingb:683682971353612334> "]
         player = msg.lower()
         print(msg)
         if player == computer:
             await ctx.send("Tie! ")
         elif player == "rock":
             if computer == "paper":
+                await asyncio.sleep(1)
                 await ctx.send(f"paper. You lose!")
                 await ctx.send(randchoice(win))
             else:
+                await asyncio.sleep(1)
                 await ctx.send("scissors. You win!")
                 await ctx.send(randchoice(lose))
         elif player == "paper":
             if computer == "scissors":
+                await asyncio.sleep(1)
                 await ctx.send("scissors. You lose!")
                 await ctx.send(randchoice(win))
             else:
+                await asyncio.sleep(1)
                 await ctx.send("rock. You win!")
                 await ctx.send(randchoice(lose))
         elif player == "scissors":
             if computer == "rock":
+                await asyncio.sleep(1)
                 await ctx.send("rock. You lose!")
                 await ctx.send(randchoice(win))
             else:
+                await asyncio.sleep(1)
                 await ctx.send("paper. You win!")
                 await ctx.send(randchoice(lose))
         else:
+            await asyncio.sleep(1)
             await ctx.send(randchoice(huh))
 
 
