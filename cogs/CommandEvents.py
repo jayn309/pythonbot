839e21c202d1ec0b93c2645fd55628156512e4f8
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-07 17:42:33 -0700

    update emotes using cmd

--- cogs/CommandEvents.py
+++ cogs/CommandEvents.py
@@ -1,5 +1,4 @@
 import discord
-import re
 from discord.ext import commands
 
 commands_tally = {}
@@ -20,44 +19,5 @@ class CommandEvents(commands.Cog):
                 commands_tally[ctx.command.name] = 1
             print(commands_tally)
 
-    @commands.Cog.listener()
-    async def on_message(self,message):
-        if message.author.id == 685307035142586380:
-            return
-        #if message is empty
-        if len(message.content)==0:
-            return
-        if (message.content[0]==":" and message.content[-1]==":") or (message.content[0]=="," and message.content[-1]==","):
-
-           #regex for finding emote matches
-            pattern = re.compile(r"\s*[:,][\w]+[:,]\s*")
-            matches = pattern.findall(message.content)
-
-            matchlength = 0
-            #if emotes found
-            if not matches == None:
-                #check if message has anything other than emotes and spaces
-                for x in matches:
-                    matchlength += len(x)
-
-                #if it doesn't then put together list of emotes to send
-                if matchlength == len(message.content):
-
-                    #print(f"message has nothing but emotes. {len(message.content)}={matchlength}")
-                    emotes = self.client.emojis
-                    finalmsg=""
-                    for x in matches:
-                        requestedemoji = x.strip()[1:-1]
-                        for i in range(len(emotes)):
-                            if emotes[i].name == requestedemoji:
-                                finalmsg+=((str)(emotes[i]))
-                                break
-                    #if finalmsg isn't empty then send
-                    if len(finalmsg)>0:
-                        await message.channel.send(finalmsg)
-                    return
-                else:
-                    pass
-                    #print(f"message has things other than emotes. {len(message.content)}!={matchlength}")
 def setup(client):
     client.add_cog(CommandEvents(client))
--- cogs/Emotes.py
+++ cogs/Emotes.py
@@ -1,4 +1,5 @@
 import discord
+import re
 from discord.ext import commands
 from typing import List
 
@@ -7,6 +8,45 @@ class Emotes(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        if message.author.id == 685307035142586380:
+            return
+        #if message is empty
+        if len(message.content)==0:
+            return
+        if (message.content[0]==":" and message.content[-1]==":") or (message.content[0]=="," and message.content[-1]==","):
+
+           #regex for finding emote matches
+            pattern = re.compile(r"\s*[:,][\w]+[:,]\s*")
+            matches = pattern.findall(message.content)
+
+            matchlength = 0
+            #if emotes found
+            if not matches == None:
+                #check if message has anything other than emotes and spaces
+                for x in matches:
+                    matchlength += len(x)
+
+                #if it doesn't then put together list of emotes to send
+                if matchlength == len(message.content):
+
+                    #print(f"message has nothing but emotes. {len(message.content)}={matchlength}")
+                    emotes = self.client.emojis
+                    finalmsg=""
+                    for x in matches:
+                        requestedemoji = x.strip()[1:-1]
+                        for i in range(len(emotes)):
+                            if emotes[i].name == requestedemoji:
+                                finalmsg+=((str)(emotes[i]))
+                                break
+                    #if finalmsg isn't empty then send
+                    if len(finalmsg)>0:
+                        await message.channel.send(finalmsg)
+                    return
+                else:
+                    pass
+                    #print(f"message has things other than emotes. {len(message.content)}!={matchlength}")
 
     @commands.command()
     @commands.has_guild_permissions(administrator=True)
