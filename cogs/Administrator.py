2155adffe8c4135a76604a2c513153dd20b1331f
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-09 15:43:42 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -12,6 +12,7 @@ class Administrator(commands.Cog):
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason)
         await ctx.send(f"{member.mention} got ban")
+        await ctx.message.channel.purge(2)
     @ban.error
     async def ban_error(self,ctx, error):
         if isinstance(error, commands.CheckFailure):
@@ -21,17 +22,21 @@ class Administrator(commands.Cog):
     @commands.has_guild_permissions(ban_members=True, kick_members=True)
     async def unban(self,ctx, *, member):
         banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.slpit('#')
         for ban_entry in banned_users:
             user = ban_entry.user
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned the user.')
-            return
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned the user.')
+                await ctx.message.channel.purge(2)
+                return
 
     @commands.command()
     @commands.has_guild_permissions(ban_members=True, kick_members=True)
     async def kick(self,ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(f"{member.mention} got kicked")
+        await ctx.message.channel.purge(2)
     @kick.error
     async def kick_error(self,ctx, error):
         if isinstance(error, commands.CheckFailure):
@@ -55,6 +60,7 @@ class Administrator(commands.Cog):
         else:
             await member.add_roles(role)
         await ctx.send(f"{member.mention} was muted")
+        await ctx.message.channel.purge(2)
     @mute.error
     async def mute_error(self,ctx, error):
         if isinstance(error, commands.CheckFailure):
@@ -66,6 +72,7 @@ class Administrator(commands.Cog):
         role = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.remove_roles(role)
         await ctx.send(f"{member.mention} was unmuted")
+        await ctx.message.channel.purge(2)
     @unmute.error
     async def unmute_error(self,ctx, error):
         if isinstance(error, commands.CheckFailure):
