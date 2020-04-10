133c7481eb478840209fc9ec1108fb1c6054d53b
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-09 18:12:28 -0700

    Update Log.py

--- cogs/Log.py
+++ cogs/Log.py
@@ -12,6 +12,17 @@ class Log(commands.Cog):
     async def on_member_remove(self, member):
         channel = discord.utils.get(member.guild.text_channels, name="log")
         if channel:
+            embed = discord.Embed(description=f'{len(member.guild.members)}th member joined', color=member.color)
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.set_author(name=member.name, icon_url=member.avatar_url)
+            embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
+            embed.timestamp = datetime.datetime.utcnow()
+            await channel.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name="log")
+        if channel:
             embed = discord.Embed(description='Goodbye', color=member.color)
             embed.set_thumbnail(url=member.avatar_url)
             embed.set_author(name=member.name, icon_url=member.avatar_url)
