1468f03c6048fd5f88bae84ab419c9e74e75ef62
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-09 17:49:32 -0700

    member remove

---
+++ cogs/Log.py
@@ -0,0 +1,23 @@
+import discord
+import datetime
+from random import choice
+from discord.ext import commands
+from discord.utils import get
+
+class Log(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = discord.utils.get(member.guild.channels, name="log")
+        if channel:
+            embed = discord.Embed(description='Goodbye', color=member.color)
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.set_author(name=member.name, icon_url=member.avatar_url)
+            embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
+            embed.timestamp = datetime.datetime.utcnow()
+
+
+def setup(client):
+    client.add_cog(Log(client))
