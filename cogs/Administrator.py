72b895834ff1c7c98de07bbc049026edf71134fa
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-11 17:47:16 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -109,9 +109,28 @@ class Administrator(commands.Cog):
 
     @commands.command()
     @commands.has_guild_permissions(administrator=True)
-    async def addrole(self,role: discord.Role,member: discord.Member):
-        await member.add_roles(role)
-        
+    async def role(self,ctx, role: discord.Role=None, user: discord.Member=None):
+        if role is None and user is None:
+            return await ctx.send("You haven't specified a role or a user! ")
+
+        if role not in ctx.message.guild.roles or user not in ctx.message.guild.members:
+            return await ctx.send("That role or user doesn't exist.")
+
+        if role not in ctx.message.author.roles and user == None:
+            await user.add_roles(ctx.message.author, role)
+            return await ctx.send(f'{role} role has been added to {user}.')
+
+        if role in ctx.message.author.roles and user == None:
+            await user.remove_roles(ctx.message.author, role)
+            return await ctx.send(f'{role} role has been removed from {user}.')
+
+        if  user != None and role not in user.roles:
+            await user.add_roles(user, role)
+            return await ctx.send(f"{role} role has been added to {user}.")
+
+        if  user != None and role in user.roles:
+            await user.remove_roles(user, role)
+            return await ctx.send(f"{role} role has been removed from {user}.")
 
 def setup(client):
     client.add_cog(Administrator(client))
