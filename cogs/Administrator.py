936fa39642047e18d2588b064590c33ae9ef6e64
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-14 16:30:00 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -17,8 +17,10 @@ class Administrator(commands.Cog):
             await ctx.send(f"{member.mention} got ban")
             channel = discord.utils.get(member.guild.text_channels, name='log')
             if channel:
-                embed = discord.Embed(description=f'{member.guild.members} got ban by {ctx.author}', colour=member.color)
+                embed = discord.Embed(description=f'{ctx.author} banned {member.guild.members}', colour=member.color)
+                embed.set_thumbnail(url=member.avatar_url)
                 embed.set_author(name=member.name, icon_url=member.avatar_url)
+                embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
                 embed.timestamp = datetime.datetime.utcnow()
                 await channel.send(embed=embed)
         except discord.Forbidden:
