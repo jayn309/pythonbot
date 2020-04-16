69845bbf21efc4aac929971ef68861673bc4b78a
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-16 14:37:33 -0700

    move

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -5,7 +5,6 @@ import asyncio
 from discord.ext import commands
 from random import choice as randchoice
 from random import randint, sample
-from Riddles import Questions, Answers
 
 class MicsCommands(commands.Cog):
     def __init__(self, client):
@@ -210,8 +209,19 @@ class MicsCommands(commands.Cog):
             await asyncio.sleep(1)
             await ctx.send(randchoice(huh))
 
+
     @commands.command()
     async def riddle(self,ctx):
+        Questions =['What has to be broken before you can use it?',
+            'I’m tall when I’m young, and I’m short when I’m old. What am I?',
+            'What month of the year has 28 days?', 
+            'The more of this there is, the less you see. What is it?',
+            'What has many keys but can’t open a single lock?', 
+            'Where does today come before yesterday?', 
+            'What invention lets you look right through a wall?',
+            'If you’ve got me, you want to share me; if you share me, you haven’t kept me. What am I?',
+            'It belongs to you, but other people use it more than you do. What is it?']
+        Answers =['egg','candle','all','darkness','piano','dictionary','window','secret','name']
         key = dict(zip(Questions, Answers))
         def check(m):
             try:
