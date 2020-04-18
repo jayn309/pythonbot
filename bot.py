ae17234816a3c6156651905131919390c1ba16f1
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-17 18:33:06 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ client.load_extension(f'cogs.Roles')
 client.load_extension(f'cogs.Emotes')
 client.load_extension(f'cogs.Log')
 client.load_extension(f'cogs.Riddle')
+client.load_extension(f'cogs.Pun')
 
 @client.event
 async def on_ready():
