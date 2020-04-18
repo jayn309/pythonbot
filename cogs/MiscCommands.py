97fee4cada76b29c441fa632f641fbf654239caf
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-17 17:06:24 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -133,6 +133,7 @@ class MicsCommands(commands.Cog):
             await ctx.send(f"{slotmachine} No match, you lost 😢")
 
     @commands.command()
+    @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
     async def numgame(self,ctx):
         if ctx.channel.id == 680233219303800893:
             number = random.randint(1,100)
