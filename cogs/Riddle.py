b435cc73dac48990ab37f40a511310b7b8262098
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-04-17 17:44:29 -0700

    Update Riddle.py

--- cogs/Riddle.py
+++ cogs/Riddle.py
@@ -21,9 +21,53 @@ class Riddle(commands.Cog):
                 'Where does today come before yesterday?', 
                 'What invention lets you look right through a wall?',
                 'If you’ve got me, you want to share me; if you share me, you haven’t kept me. What am I?',
-                'It belongs to you, but other people use it more than you do. What is it?']
+                'It belongs to you, but other people use it more than you do. What is it?',
+                'There are 30 cows in a field, and 28 chickens. How many didnt?',
+                'If life gets tough, what do you have that you can always count on?',
+                'What is one thing that all wise men, regardless of their politics or religion, agree is between heaven and earth?',
+                'What is easy to get into, but hard to get out of?',
+                'A seven letter word containing thousands of letters',
+                'A kind of tree can you carry in your hand?',
+                'I can fly but have no wings. I can cry but I have no eyes. Wherever I go, darkness follows me. What am I?',
+                'Feed me and I live, yet give me a drink and I die',
+                'What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?',
+                'What flies without wings?',
+                'What begins with T, ends with T and has T in it?',
+                'What can you catch but not throw?',
+                'What can travel around the world while staying in a corner?',
+                'What is as big as you are and yet does not weigh anything?',
+                'Take off my skin - I wont cry, but you will! What am I?',
+                'What has four legs, but cant walk?',
+                'You are my brother, but I am not your brother. Who am I?',
+                'Whats orange and sounds like a parrot?',
+                'Where do fish keep their money?',
+                'I am always in front and never behind. What am I?',
+                'Which word contains 26 letters but only three syllables?',
+                'What has four eyes but cant see?',
+                'What never asks questions but is always answered. What am I?',
+                'What has cities, but no houses; forests, but no trees; and water, but no fish?',
+                'What is harder to catch the faster you run?',
+                'Which vehicle is spelled the same forwards and backwards?',
+                'What type of dress can never be worn?',
+                'What loses its head in the morning but gets it back at night?',
+                'What tastes better than it smells?',
+                'What is the saddest fruit?',
+                'A barrel of water weighs 20 pounds. What must you add to it to make it weigh 12 pounds?',
+                'I turn around once. What is out will not get in. I turn around again. What is in will not get out. What am I?',
+                'What goes through a door but never goes in and never comes out?',
+                'What disappears the moment you say its name?',
+                'This company makes billions of dollars selling Windows.',
+                'What is it something that you always have but you always leave behind?',
+                'I am flat when I am new. I am fat when you use me. I release my gas when something sharp touches me. What am I?',
+                'Look at me. I can bring a smile to your face, a tear to your eye, or even a thought to your mind. But, I cant be seen. What am I?',
+                'What does December have that other months dont have?',
+                'What goes through towns and over hills but never moves?',
+                'What can be seen in the middle of March and April that cannot be seen at the beginning or end of either month?']
                 
-        Answers =['egg','candle','all','darkness','piano','dictionary','window','secret','name']
+        Answers =['egg','candle','all','darkness','piano','dictionary','window','secret','name','ten','fingers','and','trouble','mailbox',
+        'palm','clouds','fire','noon','time','teapot','cold','stamp','shadow','onion','table','sister','carrot','riverbank','future','alphabet',
+        'mississippi','doorbell','map','breath','racecar','address','pillow','tongue','blueberry','holes','key','keyhole','silence',
+        'microsoft','fingerprint','baloon','memories','d','road','r']
         if ctx.channel.id == 680233219303800893:
             i = random.choice(range(len(Questions)))
             await ctx.send(Questions[i])
@@ -37,9 +81,11 @@ class Riddle(commands.Cog):
                 try:     
                     msg = await self.client.wait_for('message',check=check,timeout=30.0)
                     if msg.content.lower() != Answers[i]:
+                        await ctx.message.delete()
                         await asyncio.sleep(1)
                         await ctx.send(f'Incorrect. Game over. Dumb Dumb! <:wendyyikes:682673361725554785>')
                     elif msg.content.lower() == Answers[i]:
+                        await ctx.message.delete()
                         await asyncio.sleep(1)
                         await ctx.send('You got it <:wensun:699102648229691402> ')
                 except asyncio.TimeoutError:
