61d78c4e980453659dbed17de464e5bd496dca1d
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-05-02 19:11:02 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -151,7 +151,10 @@ class Administrator(commands.Cog):
                     return False
         msg = await self.client.wait_for('message',check=check)
         number_of_roles = int(msg.content)
-        await privaterole_channel.send("Please type the private channels name below")
+        if number_of_roles != 0:
+            await privaterole_channel.send("Please type the private channels name below")
+        else:
+            pass
         while number_of_roles != 0:
             msg_channel_name = await self.client.wait_for('message',check=check)
             if msg_channel_name.content.lower () == 'sansisquad':
