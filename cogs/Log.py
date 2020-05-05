8d63861ee1b97165516f6e387b4631f7e810587d
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-05-04 23:15:59 -0700

    Update Log.py

--- cogs/Log.py
+++ cogs/Log.py
@@ -71,13 +71,13 @@ class Log(commands.Cog):
         if message.author.id == 685307035142586380 or message.author.id == 325387620266016768 or message.author.id == 235088799074484224 or message.author.id == 234395307759108106:
             return
         else:
-            if not message.content.attachments:
+            if not message.attachments:
                 delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.content}')
                 delete_embed.set_footer(text=f"Author ID:{message.author.id} • Created at: {message.created_at}")
                 delete_embed.timestamp = datetime.datetime.utcnow()
                 await log_channel.send(embed=delete_embed)
             else:
-                delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.content.attachments.url}')
+                delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.attachments.url}')
                 delete_embed.set_footer(text=f"Author ID:{message.author.id} • Created at: {message.created_at}")
                 delete_embed.timestamp = datetime.datetime.utcnow()
                 await log_channel.send(embed=delete_embed)
