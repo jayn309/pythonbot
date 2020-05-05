70651b82754c89c425ac1c774c89324e5b5a0bb6
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-05-04 23:14:07 -0700

    Update Log.py

--- cogs/Log.py
+++ cogs/Log.py
@@ -71,10 +71,16 @@ class Log(commands.Cog):
         if message.author.id == 685307035142586380 or message.author.id == 325387620266016768 or message.author.id == 235088799074484224 or message.author.id == 234395307759108106:
             return
         else:
-            delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.content}')
-            delete_embed.set_footer(text=f"Author ID:{message.author.id} • Created at: {message.created_at}")
-            delete_embed.timestamp = datetime.datetime.utcnow()
-            await log_channel.send(embed=delete_embed)
+            if not message.content.attachments:
+                delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.content}')
+                delete_embed.set_footer(text=f"Author ID:{message.author.id} • Created at: {message.created_at}")
+                delete_embed.timestamp = datetime.datetime.utcnow()
+                await log_channel.send(embed=delete_embed)
+            else:
+                delete_embed = discord.Embed(title=f'@{message.author.nick} deleted a message in #{message.channel}', description = f'{message.content.attachments.url}')
+                delete_embed.set_footer(text=f"Author ID:{message.author.id} • Created at: {message.created_at}")
+                delete_embed.timestamp = datetime.datetime.utcnow()
+                await log_channel.send(embed=delete_embed)
 
 
 def setup(client):
