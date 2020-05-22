f04cecbe46365cf1b6fce42c581979cccbbc5105
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-05-22 11:13:46 -0700

    Update Log.py

--- cogs/Log.py
+++ cogs/Log.py
@@ -53,7 +53,7 @@ class Log(commands.Cog):
     @commands.Cog.listener()
     async def on_message_edit(self, before,after):
         channel = discord.utils.get(before.guild.channels, name='log')
-        if before.author.id == 685307035142586380 or before.author.id == 325387620266016768 or before.author.id == 234395307759108106 or before.author.id == 235088799074484224 or before.author.id == 172002275412279296 :
+        if before.author.id == 685307035142586380 or before.author.id == 325387620266016768 or before.author.id == 234395307759108106 or before.author.id == 235088799074484224 or before.author.id == 172002275412279296 or before.author.id == 359401025330741248:
             return
         if before.content == after.content:
             return
