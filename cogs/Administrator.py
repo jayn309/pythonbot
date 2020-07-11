5a0378d78df63a0290a753d9a0712bbe526edc65
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-10 22:33:52 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -301,8 +301,8 @@ class Administrator(commands.Cog):
 
                 else:
                     guild = self.client.get_guild(626016069873696791)
-                    member_role = get(guild.roles, name='Velvified')
                     member = guild.get_member(message.author.id)
+                    member_role = get(guild.roles, name='Velvified')
                     verified = [member for member in guild.members 
                                 if member_role in message.author.roles]
                     if verified:
