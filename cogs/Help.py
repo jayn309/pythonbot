35319841de61d08f74b3a385ad57ec0435c69a46
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-14 22:43:17 -0700

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from discord import Embed
+from discord.ext import menus
 from discord.utils import get
 from discord.ext.menus import MenuPages, ListPageSource
 from discord.ext.commands import Cog
