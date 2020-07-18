43759e4420231ec207c8061d620fac4f9da98b54
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-17 17:32:01 -0700

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -66,7 +66,7 @@ class Help(Cog):
 			embed = Embed(title=f"Help with `{command}`",
 						description=syntax(command),
 						colour=ctx.author.colour)
-			embed.add_field(name="Command description", value=command.help)
+			embed.add_field(name="Command description", value='None')
 			await ctx.send(embed=embed)
 
 	@command(name="help")
