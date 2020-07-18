14540b93a44d1bd0ed065d1b632feb534cc444d1
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-18 15:59:31 -0700

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -59,10 +59,10 @@ class Help(Cog):
 		embed = Embed(title=f"Help with `{command}`",
 						description=syntax(command),
 						colour=ctx.author.colour)
-		if command.description is not None:
-			embed.add_field(name="Command description", value=command.description)
-		else:
+		if command.description is  None:
 			embed.add_field(name="Command description", value="No description.")
+		else:
+			embed.add_field(name="Command description", value=command.description)
 		await ctx.send(embed=embed)
 
 	@command(name="help")
