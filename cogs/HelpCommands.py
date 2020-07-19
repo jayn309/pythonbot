efa2d7d398facb27a1b6632dffca6e81104ca617
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-18 17:43:34 -0700

    Update HelpCommands.py

--- cogs/HelpCommands.py
+++ cogs/HelpCommands.py
@@ -38,9 +38,9 @@ class HelpCommands(commands.Cog):
             embed = discord.Embed(description='Here is the list of commands! \n For more info on a specific command use _help {command}',
                         colour = discord.Colour.blurple())
             embed.set_author(name="Commands list")
-            embed.add_field(name='👑 Admin', value="```kick``` ```ban``` ```unban``` ```mute``` ```unmute``` ```purge``` ```say``` ```edit``` ```showemotes``` ```addrole``` ```removerole``` ```move``` ```alladdrole``` ```allremoverole``` ```countr``` ```showemote```"
-                ,inline=False)
-            embed.add_field(name='🎉 Fun', value="```eightb``` ```coinflip``` ```f``` ```choose``` ```hug``` ```rate``` ```hot``` ```slot``` ```tableflip``` ```unflip``` ```add``` ```subtract``` ```multiply``` ```divide``` ```avatar``` ```usersinfo``` ```numgame``` ```rps``` ```riddle``` ```pun``` ```google``` ```youtube```"
+            embed.add_field(name='👑 Admin', value="kick ban unban mute unmute purge say edit showemotes addrole removerole move alladdrole allremoverole countr showemote"
+                ,inline=True)
+            embed.add_field(name='🎉 Fun', value="eightb coinflip f choose hug rate hot slot tableflip unflip add subtract multiply divide avatar usersinfo numgame rps riddle pun google youtube"
                 ,inline=False)
             await ctx.send(author.mention, embed=embed)
         
