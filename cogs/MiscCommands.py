fa1a279f9ae5fe10fc6d31997f3189fc766267c6
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-07-22 20:01:11 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -322,17 +322,13 @@ class MicsCommands(commands.Cog):
         if lang is None:
             await ctx.send('Please provide a language.')
         else:
-            if lang in LANGCODES or LANGUAGES:
-                try:
-                    t = Translator()
-                    a = t.translate(args, dest=lang)
-                    await ctx.send(a.text)
-                except ValueError:
-                    await ctx.send('Invalid language.')
-                    return
-            else:
+            try:
+                t = Translator()
+                a = t.translate(args, dest=lang)
+                await ctx.send(a.text)
+            except ValueError:
+                await ctx.send('Invalid language.')
                 return
-                    
 
 def setup(client):
     client.add_cog(MicsCommands(client))
