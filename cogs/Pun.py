38d815da3c98ad40ba218d33d1ce35d47dceed4d
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-03 16:15:08 -0700

    Update Pun.py

--- cogs/Pun.py
+++ cogs/Pun.py
@@ -85,16 +85,13 @@ class Pun(commands.Cog):
                 
         try:     
             msg = await self.client.wait_for('message',check=check,timeout=30.0)
-            await ctx.channel.purge(limit=3)
-            if msg.content.startswith(':'):
-                await ctx.send(f'Answer the question!!!')
-            else:
-                if msg.content.lower() != Answers[i]:
-                    await asyncio.sleep(1)
-                    await ctx.send(f'Incorrect. Game over. Dumb Dumb! <:wendyyikes:682673361725554785>')
-                elif msg.content.lower() == Answers[i]:
-                    await asyncio.sleep(1)
-                    await ctx.send('You got it <:wensun:699102648229691402> ')
+            await ctx.channel.purge(limit=3) 
+            if msg.content.lower() != Answers[i]:
+                await asyncio.sleep(1)
+                await ctx.send(f'Incorrect. Game over. Dumb Dumb! <:wendyyikes:682673361725554785>')
+            elif msg.content.lower() == Answers[i]:
+                await asyncio.sleep(1)
+                await ctx.send('You got it <:wensun:699102648229691402> ')
         except asyncio.TimeoutError:
             await ctx.send('Oops! Nobody solved it.')
             await asyncio.sleep(1)
