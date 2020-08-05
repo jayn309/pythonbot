afb310782b1af84625152f9bbdc4044939bfd675
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-04 19:49:47 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -374,7 +374,7 @@ class MicsCommands(commands.Cog):
             url = base_url.format(match.group(2))
             await ctx.send(f'{url}')
         elif amatch:
-            x = re.search(r'<(\d+)>', emoji)
+            x = re.search(r'<(\w+)>', emoji)
             try:
                 await ctx.send(x.group())
             except AttributeError:
