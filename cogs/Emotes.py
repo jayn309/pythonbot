42a429396f756d8cce0e1c637ca6e2464dd3bb60
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-18 20:10:56 -0700

    Update Emotes.py

--- cogs/Emotes.py
+++ cogs/Emotes.py
@@ -107,10 +107,10 @@ class Emotes(commands.Cog):
         amatch = re.match(r'<(\w):(\w+):(\d+)>', str(emoji))
         if emoji is None:
             async for message in ctx.channel.history(limit = 10):
-                if message.content is match:
+                if match in message.content.lower():
                     url = base_url.format(match.group(2))
                     await ctx.send(f'{url}')
-                elif message.content is amatch:
+                elif amatch in message.content.lower():
                     x = re.search(r':(\d+)', str(emoji))
                     aurl = animated_url.format(x.group(1))
                     await ctx.send(f'{aurl}')
