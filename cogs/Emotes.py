10b6fe0bff42b82811c8c635dd62da246118eb6c
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-19 15:53:02 -0700

    Update Emotes.py

--- cogs/Emotes.py
+++ cogs/Emotes.py
@@ -100,7 +100,7 @@ class Emotes(commands.Cog):
 
     @commands.command(aliases=['el','l'],brief='get an enlarged version of an emote')
     @commands.guild_only()
-    async def enlarge(self, ctx, emoji: str):
+    async def enlarge(self, ctx, emoji: str =None):
         base_url = 'https://cdn.discordapp.com/emojis/{}.png?v=1'
         match = re.match(r'<:(\w+):(\d+)>', emoji)
         animated_url = 'https://cdn.discordapp.com/emojis/{}.gif?v=1'
@@ -117,10 +117,12 @@ class Emotes(commands.Cog):
                 await ctx.send(f'``{emoji}`` is not an emoji')
         else:
             for message in ctx.channel.history(limit = 10, oldest_first = False):
-                if message.search(message.content) is match:
+                nmatch = re.search(r'<:(\w+):(\d+)>',message)
+                namatch = re.search(r'<(\w):(\w+):(\d+)>', message)
+                if nmatch:
                     url = base_url.format(match.group(2))
                     await ctx.send(f'{url}')
-                elif message.search(message.content) is amatch:
+                elif namatch:
                     x = re.search(r':(\d+)', emoji)
                     aurl = animated_url.format(x.group(1))
                     await ctx.send(f'{aurl}')
