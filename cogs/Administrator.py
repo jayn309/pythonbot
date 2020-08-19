bc712a4f467d2ca79b4ede33682aad0e6c5ffeb5
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-18 17:27:03 -0700

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -34,7 +34,7 @@ class TimeConverter(commands.Converter):
 class Administrator(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.load_extension(name='jishaku')
+		self.bot.load_extension(name='jishaku')
 
 	@commands.command(brief='ban a member')
 	@commands.has_guild_permissions(ban_members=True, kick_members=True)
