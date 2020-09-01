1e60242f2cf25429a1adb757df49b6a280603b35
jayn309 <62487369+jayn309@users.noreply.github.com>
2020-08-31 19:33:40 -0700

    Update MiscCommands.py

--- cogs/MiscCommands.py
+++ cogs/MiscCommands.py
@@ -396,7 +396,55 @@ class MicsCommands(commands.Cog):
         for emoji in reaction:
             await message.add_reaction(emoji)
 
-    
+    @commands.command(aliases=["p","mkpoll","mp"],brief="Start a poll for a set time\nUse quotation mark to separate question")
+    @commands.has_guild_permissions(manage_guild=True)
+    @commands.guild_only()
+    async def poll(self, ctx, seconds: int, question: str, *options):
+        numbers = ("1️⃣", "2⃣", "3⃣", "4⃣", "5⃣",
+		   "6⃣", "7⃣", "8⃣", "9⃣", "🔟")
+
+        if len(options) > 10:
+            await ctx.send("You can only supply a maximum of 10 options.")
+
+        else:
+            embed = Embed(title="Poll",
+						  description=question,
+						  colour=ctx.author.colour,
+						  timestamp=datetime.datetime.utcnow())
+
+            fields = [("Options", "\n".join([f"{numbers[idx]} {option}" for idx, option in enumerate(options)]), False),
+					  ("Instructions", "React to cast a vote!", False)]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            message = await ctx.send(embed=embed)
+
+            for emoji in numbers[:len(options)]:
+                await message.add_reaction(emoji)
+
+            self.polls.append((message.channel.id, message.id))
+            self.bot.scheduler.add_job(self.complete_poll, "date", run_date=datetime.datetime.now()+timedelta(seconds=seconds),
+									   args=[message.channel.id, message.id])
+
+    async def complete_poll(self, channel_id, message_id):
+        message = await self.bot.get_channel(channel_id).fetch_message(message_id)
+
+        most_voted = max(message.reactions, key=lambda r: r.count)
+
+        await message.channel.send(f"The results are in and option {most_voted.emoji} was the most popular with {most_voted.count-1:,} votes!")
+        self.polls.remove((message.channel.id, message.id))
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self,payload):
+        if payload.message_id in (poll[1] for poll in self.polls):
+            message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+            for reaction in message.reactions:
+                if (not payload.member.bot
+                    and payload.member in await reaction.users().flatten()
+                    and reaction.emoji != payload.emoji.name):
+                    await message.remove_reaction(reaction.emoji, payload.member)
 
 def setup(bot):
     bot.add_cog(MicsCommands(bot))
