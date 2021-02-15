2a412caa0b8718665c5c23513408a7734dfc5474
jayn309 <62487369+jayn309@users.noreply.github.com>
2021-02-14 21:19:13 -0800

    Update Roles.py

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -36,8 +36,6 @@ class Roles(commands.Cog):
                 role = discord.utils.get(guild.roles, name='Ensign')
             if payload.emoji.name == 'irenemunch':
                 role = discord.utils.get(guild.roles, name='Camping House')
-            if payload.emoji.name == 'ireneevilsmile':
-                role = discord.utils.get(guild.roles, name='Yadong Yaseol')
 
             if role is not None:
                 member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
@@ -53,6 +51,31 @@ class Roles(commands.Cog):
                         await channel.send(f'Role was added.')
                         await asyncio.sleep(2)
                         await channel.purge(limit=1)
+                    msg = await channel.fetch_message(payload.message_id)
+                    await msg.remove_reaction(payload.emoji,payload.member)
+                else:
+                    print("Member not found.")
+            else:
+                print("Role not found.")
+
+        if  message_id == 684015060888453148:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
+
+            if payload.emoji.name == 'ireneevilsmile':
+                role = discord.utils.get(guild.roles, name='Yadong Yaseol')
+
+            if role is not None:
+                member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+                channel = discord.utils.find(lambda c : c.id == channel_id, guild.channels)
+                if member is not None:
+                    if role in member.roles:
+                        await member.remove_roles(role)
+                        await channel.send(f'Role was removed.')
+                        await asyncio.sleep(2)
+                        await channel.purge(limit=1)
+                    else:
+                        await channel.send(f'You do not have this role.')
                     msg = await channel.fetch_message(payload.message_id)
                     await msg.remove_reaction(payload.emoji,payload.member)
                 else:
